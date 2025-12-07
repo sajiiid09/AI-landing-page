4145db0ab966e89cf54efe945697319e24c9f0f8
Sajid Mahmud <134144226+sajiiid09@users.noreply.github.com>
2025-12-08 02:33:04 +0600

    Add sticky scroll text reveal section

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,26 @@
 import Encryption from "@/components/main/Encryption";
 import Hero from "@/components/main/Hero";
 // import Projects from "@/components/main/Projects";
+import ScrollTextSection from "@/components/main/ScrollTextSection";
 import Skills from "@/components/main/Skills";
 import StarsCanvas from "@/components/main/StarBackground";
 
 export default function Home() {
   return (
-    <main className="h-full w-full">
-      <Hero />
+    <main className="h-full w-full bg-black">
+      <div className="flex flex-col gap-20">
+        <Hero />
+        <ScrollTextSection />
 
-      <section className="relative">
-        <StarsCanvas />
-        <div className="relative z-10 flex flex-col gap-20">
-          <Skills />
-          <Encryption />
-          {/* <Projects /> */}
-        </div>
-      </section>
+        <section className="relative">
+          <StarsCanvas />
+          <div className="relative z-10 flex flex-col gap-20">
+            <Skills />
+            <Encryption />
+            {/* <Projects /> */}
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
